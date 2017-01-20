eab4d436c339ce3bdb84e708161ae30b6902528b
phaninittala <phaninittala@gmail.com>
2017-01-20 14:50:20 +0530

    Add files via upload

---
+++ example.java
@@ -0,0 +1,12 @@
+ class example {
+ int x; int y;
+ String name;
+ public static void main(String args[]) {
+ example pnt = new example();
+ System.out.println ("pnt is " + pnt.name + " " + pnt.x + " " + pnt.y);
+}
+ }
+
+ 
+ //result
+ //Option D. The program prints pnt is Null 0 0 as x,y,name variables are not assigned any values.
